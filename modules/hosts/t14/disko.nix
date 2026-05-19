cf286d88b699747fbb152880f0fa1c4e8d276a87
csman964 <mdi2611@hotmail.com>
2026-05-19 22:42:45 +0200

    first commit

---
+++ modules/hosts/t14/disko.nix
@@ -0,0 +1,96 @@
+{
+  disko.devices = {
+    disk = {
+      nvme0 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+
+        content = {
+          type = "gpt";
+
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              size = "512M";
+              type = "EF00";
+
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+
+            swap = {
+              size = "32G";
+
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+
+            root = {
+              size = "100%";
+
+              content = {
+                type = "btrfs";
+
+                extraArgs = [ "-f" ];
+
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                    ];
+                  };
+
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                    ];
+                  };
+
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                    ];
+                  };
+
+                  "@var" = {
+                    mountpoint = "/var";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                    ];
+                  };
+
+                  "@log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
