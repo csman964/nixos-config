704cad8c191a5b2b4d4f55d0a8283cce37d6b87b
csman964 <mdi2611@hotmail.com>
2026-05-28 15:54:47 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        flake-parts.url = "github:hercules-ci/flake-parts";
-        import-tree.url = "github:vic/import-tree";
+        #flake-parts.url = "github:hercules-ci/flake-parts";
+        #import-tree.url = "github:vic/import-tree";
         wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
         home-manager = {
             url = "github:nix-community/home-manager";
@@ -16,6 +16,41 @@
         };
     };
 
-    outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, disko }: {
+        nixosConfigurations = {
+            t14 = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = {
+            username = "csman";
+            hostname = "t14";
+            pkgs-unstable = import nixpkgs-unstable {
+                system = "x86_64-linux";
+                config.allowUnfree = true;
+            };
+            };
+            modules = [
+            ./modules/hosts/t14/configuration.nix
+            ./modules/hosts/t14/disko.nix
+            disko.nixosModules.disko
+            ./modules/core
+            ./modules/features
+            ./modules/home
+
+            home-manager.nixosModules.home-manager
+            {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "backup";
+                home-manager.extraSpecialArgs = {
+                username = "csman";
+                };
+                home-manager.users.csman = import ./modules/home/home.nix;
+            }
+            ];
+        };
+
+
+        };
+    };
 
 }
