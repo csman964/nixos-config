df42c31d0dace2133e869b03bcbb67ba5a266a4a
csman964 <mdi2611@hotmail.com>
2026-09-10 21:06:59 +0200

    fix modify permission

--- hosts/t14/samba-client.nix
+++ hosts/t14/samba-client.nix
@@ -3,24 +3,13 @@
 {
   # For mount.cifs, required unless domain name resolution is not needed.
   environment.systemPackages = [ pkgs.cifs-utils ];
-
   fileSystems."/mnt/csman" = {
     device = "//192.168.35.200/csman";
     fsType = "cifs";
-
     options = let
-      automount_opts =
-        "x-systemd.automount,noauto,"
-        + "x-systemd.idle-timeout=60,"
-        + "x-systemd.device-timeout=5s,"
-        + "x-systemd.mount-timeout=5s";
-    in [
-      "${automount_opts},"
-      + "credentials=/etc/nixos/smb-secrets,"
-      + "uid=1000,"
-      + "gid=100,"
-      + "file_mode=0664,"
-      + "dir_mode=0775"
-    ];
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100,file_mode=0664,dir_mode=0775"];
   };
 }
